148376dca8ef4ef1ab9ee9bbd864466a1e531ae1
meryvl <meryvalerolaguna@gmail.com>
2023-01-30 20:23:30 +0000

    my commit

--- src/app.js
+++ src/app.js
@@ -8,4 +8,27 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+
+  let what = [" eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function randomSentence(arr1, arr2, arr3) {
+    for (let i = 0; i < who.length; i++) {
+      who[i] = who[i][0].toUpperCase() + who[i].substr(1);
+    }
+    const number = Math.floor(Math.random() * arr1.length);
+    const number2 = Math.floor(Math.random() * arr2.length);
+    const number3 = Math.floor(Math.random() * arr3.length);
+    const sentence = arr1[number] + " " + arr2[number2] + " " + arr3[number3];
+    return sentence;
+  }
+
+  document.querySelector("#excuse").innerHTML = randomSentence(who, what, when);
 };
